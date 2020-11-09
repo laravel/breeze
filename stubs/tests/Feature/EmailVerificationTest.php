e2b6643978739f2e4a51deda8bcd03228fcddc0a
Taylor Otwell <taylorotwell@gmail.com>
2020-11-09 13:54:20 -0600

    wip

--- stubs/tests/Feature/EmailVerificationTest.php
+++ stubs/tests/Feature/EmailVerificationTest.php
@@ -20,7 +20,7 @@ class EmailVerificationTest extends TestCase
             'email_verified_at' => null,
         ]);
 
-        $response = $this->actingAs($user)->get('/email/verify');
+        $response = $this->actingAs($user)->get('/verify-email');
 
         $response->assertStatus(200);
     }
