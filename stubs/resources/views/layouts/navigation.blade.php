633b046c9ec0df72e6479e9cd3a2879ac50f016e
Taylor Otwell <taylorotwell@gmail.com>
2020-11-10 07:59:30 -0600

    update nav logo

--- stubs/resources/views/layouts/navigation.blade.php
+++ stubs/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto" />
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
