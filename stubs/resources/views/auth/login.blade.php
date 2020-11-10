f519297371db4a92ee161a004741030bb82972c5
James Brooks <james@alt-three.com>
2020-11-10 13:48:19 +0000

    [1.x] Application logo component (#6)
    * Rename auth-logo to application-logo and use $attributes
    
    * Use application-logo in navigation

--- stubs/resources/views/auth/login.blade.php
+++ stubs/resources/views/auth/login.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <x-auth-logo />
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
         </x-slot>
 
         <!-- Session Status -->
--- stubs/resources/views/layouts/navigation.blade.php
+++ stubs/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        Laravel
+                        <x-application-logo class="block h-9 w-auto" />
                     </a>
                 </div>
 
