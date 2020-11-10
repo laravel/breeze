a42a6ff3f5fb0cdbaea6b856ea1aacaba0a9fb56
Wojciech Gabryś <info@wojciechgabrys.com>
2020-11-10 22:49:32 +0100

    Restoring old input values after failed validation

--- stubs/App/Http/Controllers/Auth/PasswordResetLinkController.php
+++ stubs/App/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -41,6 +41,7 @@ class PasswordResetLinkController extends Controller
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
-                    : back()->withErrors(['email' => __($status)]);
+                    : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
     }
 }
--- stubs/resources/views/auth/register.blade.php
+++ stubs/resources/views/auth/register.blade.php
@@ -16,14 +16,14 @@
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" required autofocus />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ old('name') }}" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" required />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ old('email') }}" required />
             </div>
 
             <!-- Password -->
