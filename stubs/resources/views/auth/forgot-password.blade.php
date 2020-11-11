588fbdf5094a8f1b1de17d3c715dd2397a8b8b1a
Wojciech Gabryś <wojciechgabrys@users.noreply.github.com>
2020-11-11 14:49:47 +0100

    Restoring old input values after a failed validation (#9)
    * Restoring old input values after failed validation
    
    * Making styleci happy

--- stubs/resources/views/auth/forgot-password.blade.php
+++ stubs/resources/views/auth/forgot-password.blade.php
@@ -23,7 +23,7 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ old('email') }}" required autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
