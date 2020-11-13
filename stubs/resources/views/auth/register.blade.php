c01bef7e298feb818427e5689948dd6ad577979f
Taylor Otwell <taylor@laravel.com>
2020-11-13 08:39:48 -0600

    Update register.blade.php

--- stubs/resources/views/auth/register.blade.php
+++ stubs/resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already Registered?') }}
+                    {{ __('Already registered?') }}
                 </a>
 
                 <x-button class="ml-4">
