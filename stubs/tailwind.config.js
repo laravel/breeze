fb835fb23be5861291a7fae8d650ee18d868ca4b
Neptune Design <fabrice.fortez@gmail.com>
2020-11-23 15:00:22 +0100

    Update to Tailwind CSS v2

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -15,5 +15,5 @@ module.exports = {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
     },
 
-    plugins: [require('@tailwindcss/ui')],
+    plugins: [require('@tailwindcss/forms')],
 };
