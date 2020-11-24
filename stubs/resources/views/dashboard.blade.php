56d9043559ba68d104757708090543e4d3713e05
Taylor Otwell <taylorotwell@gmail.com>
2020-11-24 10:39:41 -0600

    change shadow

--- stubs/resources/views/dashboard.blade.php
+++ stubs/resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
