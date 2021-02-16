1e8a040510c067d7b47b580919dbe1ccef305d29
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-02-16 18:37:23 +0000

    Apply fixes from StyleCI

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php'
+        './resources/views/**/*.blade.php',
     ],
 
     theme: {
