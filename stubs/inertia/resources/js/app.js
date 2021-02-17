8dbd7495d7d8a5276b90a44762562d645df6a178
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-02-17 23:08:42 +0000

    Apply fixes from StyleCI

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -3,7 +3,7 @@ require('./bootstrap');
 // Import modules...
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress'
+import { InertiaProgress } from '@inertiajs/progress';
 
 const el = document.getElementById('app');
 
