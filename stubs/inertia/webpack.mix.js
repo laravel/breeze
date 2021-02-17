52b19f9273b7d0500d6f71514289ec779266f8f6
Taylor Otwell <taylor@laravel.com>
2021-02-17 10:48:04 -0600

    Apply fixes from StyleCI (#43)

--- stubs/inertia/webpack.mix.js
+++ stubs/inertia/webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
