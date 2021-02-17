e2ae8b17e7f3210678418275dadadfc785d1b43b
Ninja <yazjallad@gmail.com>
2021-02-17 15:08:34 -0800

    Add @inertiajs/progress to package dependencies (#46)
    * add @inertiajs/progress to the base scaffold
    
    * Update app.js
    
    * Update InstallCommand.php
    
    * Update app.js
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 // Import modules...
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress'
 
 const el = document.getElementById('app');
 
@@ -16,3 +17,10 @@ createApp({
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
     .mount(el);
+
+InertiaProgress.init({
+    delay: 250,
+    color: '#4B5563',
+    includeCSS: true,
+    showSpinner: false,
+});
