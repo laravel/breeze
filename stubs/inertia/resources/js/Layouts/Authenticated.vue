d8faa0c7b1f3c819c9ad69a306232e54009241c5
Claudio Dekker <1752195+claudiodekker@users.noreply.github.com>
2021-02-18 16:02:36 +0100

    Another <slot /> consistency fix

--- stubs/inertia/resources/js/Layouts/Authenticated.vue
+++ stubs/inertia/resources/js/Layouts/Authenticated.vue
@@ -89,7 +89,7 @@
             <!-- Page Heading -->
             <header class="bg-white shadow" v-if="$slots.header">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header"></slot>
+                    <slot name="header" />
                 </div>
             </header>
 
