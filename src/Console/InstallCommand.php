0e3f5e6d167e52d169172097756bd52245821a33
Ninja <yazjallad@gmail.com>
2021-04-30 10:01:12 -0700

    add `jsconfig.js` (#66)

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -161,6 +161,7 @@ class InstallCommand extends Command
         copy(__DIR__.'/../../stubs/inertia/tailwind.config.js', base_path('tailwind.config.js'));
         copy(__DIR__.'/../../stubs/inertia/webpack.mix.js', base_path('webpack.mix.js'));
         copy(__DIR__.'/../../stubs/inertia/webpack.config.js', base_path('webpack.config.js'));
+        copy(__DIR__.'/../../stubs/inertia/jsconfig.json', base_path('jsconfig.json'));
         copy(__DIR__.'/../../stubs/inertia/resources/css/app.css', resource_path('css/app.css'));
         copy(__DIR__.'/../../stubs/inertia/resources/js/app.js', resource_path('js/app.js'));
 
