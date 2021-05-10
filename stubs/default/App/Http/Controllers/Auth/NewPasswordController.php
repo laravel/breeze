8358489748ea78d65a9f210bd7783b19626322ca
Alexander Shchukin <sashsvamir@users.noreply.github.com>
2021-05-10 15:38:08 +0300

    Update NewPasswordController.php
    redundant parameter passing

--- stubs/default/App/Http/Controllers/Auth/NewPasswordController.php
+++ stubs/default/App/Http/Controllers/Auth/NewPasswordController.php
@@ -43,7 +43,7 @@ class NewPasswordController extends Controller
         // will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
+            $request->only('email', 'password', 'token'),
             function ($user) use ($request) {
                 $user->forceFill([
                     'password' => Hash::make($request->password),
