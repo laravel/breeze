2811010c51aab620f77f5bf3063dc6ee4d0e55a1
Jonathan Reinink <jonathan@reinink.ca>
2021-05-11 09:03:06 -0400

    Use boolean() instead of filled()

--- stubs/default/App/Http/Requests/Auth/LoginRequest.php
+++ stubs/default/App/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->filled('remember'))) {
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
