cc7040c6d688bf769517adef9bcf3aa99be0b519
Taylor Otwell <taylorotwell@gmail.com>
2021-05-19 15:52:02 -0500

    wip

--- stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.js
+++ stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.js
@@ -24,8 +24,7 @@ export default function ForgotPassword({ status }) {
     return (
         <Guest>
             <div className="mb-4 text-sm text-gray-500 leading-normal">
-                Forgot your password? No problem. Just let us know your email address and we will email you a password
-                reset link that will allow you to choose a new one.
+                Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
             </div>
 
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
