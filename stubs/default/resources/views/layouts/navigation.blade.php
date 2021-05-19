c1f9ef3b978a8873b3eda1c0cf9ef72401a53475
Taylor Otwell <taylorotwell@gmail.com>
2021-05-19 15:49:06 -0500

    fix logout wording

--- stubs/default/resources/views/layouts/navigation.blade.php
+++ stubs/default/resources/views/layouts/navigation.blade.php
@@ -41,7 +41,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log out') }}
+                                {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -91,7 +91,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log out') }}
+                        {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
