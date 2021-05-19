359b0c20cf2f10b6430d93c917bf2c4ca48e30ea
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-05-19 20:52:13 +0000

    Apply fixes from StyleCI

--- stubs/inertia-react/resources/js/Pages/Auth/VerifyEmail.js
+++ stubs/inertia-react/resources/js/Pages/Auth/VerifyEmail.js
@@ -16,7 +16,8 @@ export default function VerifyEmail({ status }) {
     return (
         <Guest>
             <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                Thanks for signing up! Before getting started, could you verify your email address by clicking on the
+                link we just emailed to you? If you didn't receive the email, we will gladly send you another.
             </div>
 
             {status === 'verification-link-sent' && (
