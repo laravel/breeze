81c900714ba149533c08ab21e18d9af655a6066a
Mior Muhammad Zaki <crynobone@gmail.com>
2021-05-19 21:29:49 +0800

    Use new Password::defaults() feature. (#71)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- stubs/inertia/app/Http/Controllers/Auth/RegisteredUserController.php
+++ stubs/inertia/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => ['required', 'confirmed', Rules\Password::min(8)],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
