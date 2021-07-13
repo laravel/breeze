eba8457b2e16d92fb0909e6e4a36f7cf9f50bc78
Taylor Otwell <taylorotwell@gmail.com>
2021-07-13 13:57:53 -0500

    update url

--- stubs/inertia-common/resources/views/app.blade.php
+++ stubs/inertia-common/resources/views/app.blade.php
@@ -21,7 +21,7 @@
         @inertia
 
         @env ('local')
-            <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
+            <script src="http://localhost:8080/js/bundle.js"></script>
         @endenv
     </body>
 </html>
