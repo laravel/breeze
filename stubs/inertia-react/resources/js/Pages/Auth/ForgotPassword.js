564ce63bd27c82ef028d21c14356afe86fbbaf42
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:38:57 -0500

    Update ForgotPassword.js

--- stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.js
+++ stubs/inertia-react/resources/js/Pages/Auth/ForgotPassword.js
@@ -23,6 +23,7 @@ export default function ForgotPassword({ status }) {
     return (
         <Guest>
             <Head title="Forgot Password" />
+
             <div className="mb-4 text-sm text-gray-500 leading-normal">
                 Forgot your password? No problem. Just let us know your email address and we will email you a password
                 reset link that will allow you to choose a new one.
