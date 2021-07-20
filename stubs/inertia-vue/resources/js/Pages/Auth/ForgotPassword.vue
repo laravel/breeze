f64da634d66e077fd9723d4817cf11ecaad4ad79
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:42:07 -0500

    Update ForgotPassword.vue

--- stubs/inertia-vue/resources/js/Pages/Auth/ForgotPassword.vue
+++ stubs/inertia-vue/resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,5 +1,6 @@
 <template>
     <Head title="Forgot Password" />
+
     <div class="mb-4 text-sm text-gray-600">
         Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
     </div>
