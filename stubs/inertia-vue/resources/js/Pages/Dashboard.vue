3219a7cf8e710d802a34ca0636ba2cd4acf2910a
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:43:07 -0500

    Update Dashboard.vue

--- stubs/inertia-vue/resources/js/Pages/Dashboard.vue
+++ stubs/inertia-vue/resources/js/Pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <Head title="Dashboard" />
+
     <breeze-authenticated-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
