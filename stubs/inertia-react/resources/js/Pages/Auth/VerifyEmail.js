345fca89e44834dfa51ffd9f7c94e80cf8b48835
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:39:46 -0500

    Update VerifyEmail.js

--- stubs/inertia-react/resources/js/Pages/Auth/VerifyEmail.js
+++ stubs/inertia-react/resources/js/Pages/Auth/VerifyEmail.js
@@ -15,6 +15,7 @@ export default function VerifyEmail({ status }) {
     return (
         <Guest>
             <Head title="Email Verification" />
+
             <div className="mb-4 text-sm text-gray-600">
                 Thanks for signing up! Before getting started, could you verify your email address by clicking on the
                 link we just emailed to you? If you didn't receive the email, we will gladly send you another.
