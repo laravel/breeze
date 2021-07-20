72c6bc4338bc98cb49bd90750f276e04c81e4dcf
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:43:20 -0500

    Update Welcome.vue

--- stubs/inertia-vue/resources/js/Pages/Welcome.vue
+++ stubs/inertia-vue/resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <template>
     <Head title="Welcome" />
+
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             <Link v-if="$page.props.auth.user" href="/dashboard" class="text-sm text-gray-700 underline">
