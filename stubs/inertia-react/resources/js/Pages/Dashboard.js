b18a300f091d911a45da9fdaaaea380696634775
Taylor Otwell <taylor@laravel.com>
2021-07-20 08:40:19 -0500

    Update Dashboard.js

--- stubs/inertia-react/resources/js/Pages/Dashboard.js
+++ stubs/inertia-react/resources/js/Pages/Dashboard.js
@@ -10,6 +10,7 @@ export default function Dashboard(props) {
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
+
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
