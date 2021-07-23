706ef81f7d3cae4be1310d55ac9a71f1454808be
Joshua Pack <joshua@hightekdesigns.com>
2021-07-22 21:59:00 -0400

    Add missing href

--- stubs/inertia-vue/resources/js/Components/DropdownLink.vue
+++ stubs/inertia-vue/resources/js/Components/DropdownLink.vue
@@ -1,5 +1,5 @@
 <template>
-    <Link class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+    <Link :href="href" class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
         <slot />
     </Link>
 </template>
@@ -10,6 +10,8 @@ import { Link } from '@inertiajs/inertia-vue3';
 export default {
     components: {
         Link,
-    }
+    },
+    
+    props: ['href'],
 }
 </script>
