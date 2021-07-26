b3435966a69075a6c9c42d00960f4f33eb013986
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2021-07-26 19:33:37 +0700

    Update app layout to make optional for header.
    Give optional to the header so we can make component without header required.

--- stubs/default/resources/views/layouts/app.blade.php
+++ stubs/default/resources/views/layouts/app.blade.php
@@ -19,13 +19,14 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+            @isset($header)
+                <!-- Page Heading -->
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
 
             <!-- Page Content -->
             <main>
