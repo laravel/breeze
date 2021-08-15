ebcd58fb4c7c2a831f647ee925429faa22466f80
Maarten Buis <buismaarten@outlook.com>
2021-08-15 19:33:55 +0200

    Update RegisteredUserController.php

--- stubs/default/App/Http/Controllers/Auth/RegisteredUserController.php
+++ stubs/default/App/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,8 +34,8 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
