d8f24ff93f897a3ef9ebe1379c3bd7caced3bd6f
Propaganistas <Propaganistas@users.noreply.github.com>
2021-10-17 11:16:12 +0200

    Use passwordConfirmed() method

--- stubs/default/App/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ stubs/default/App/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -37,7 +37,7 @@ class ConfirmablePasswordController extends Controller
             ]);
         }
 
-        $request->session()->put('auth.password_confirmed_at', time());
+        $request->session()->passwordConfirmed();
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
