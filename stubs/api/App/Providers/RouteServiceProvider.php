a8e35c49f0f27ceb345ed720a8bd33afd35ab7bc
Taylor Otwell <taylor@laravel.com>
2021-11-26 15:32:33 -0600

    wip

--- stubs/api/App/Providers/RouteServiceProvider.php
+++ stubs/api/App/Providers/RouteServiceProvider.php
@@ -41,6 +41,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
         });
     }
 
--- stubs/api/routes/web.php
+++ stubs/api/routes/web.php
@@ -4,15 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+require __DIR__.'/auth.php';
