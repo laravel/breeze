b7a5500d11f1addfee78c6ecbd3fee092d0e7c20
Taylor Otwell <taylor@laravel.com>
2021-11-30 12:45:16 -0600

    wip

--- stubs/api/routes/api.php
+++ stubs/api/routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
--- stubs/api/routes/web.php
+++ stubs/api/routes/web.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
 require __DIR__.'/auth.php';
