95aca5174a047ef7166b58d31c74eae3c3a0fb16
Taylor Otwell <taylor@laravel.com>
2021-11-30 12:22:06 -0600

    wip

--- src/Console/InstallsApiStack.php
+++ src/Console/InstallsApiStack.php
@@ -23,7 +23,12 @@ trait InstallsApiStack
         $files->copyDirectory(__DIR__.'/../../stubs/api/App/Http/Middleware', app_path('Http/Middleware'));
 
         $this->replaceInFile('// \Laravel\Sanctum\Http', '\Laravel\Sanctum\Http', app_path('Http/Kernel.php'));
-        $this->replaceInFile('\Illuminate\Auth\Middleware\EnsureEmailIsVerified::class', '\App\Http\Middleware\EnsureEmailIsVerified::class', app_path('Http/Kernel.php'));
+
+        $this->replaceInFile(
+            '\Illuminate\Auth\Middleware\EnsureEmailIsVerified::class',
+            '\App\Http\Middleware\EnsureEmailIsVerified::class',
+            app_path('Http/Kernel.php')
+        );
 
         // Requests...
         $files->ensureDirectoryExists(app_path('Http/Requests/Auth'));
@@ -31,6 +36,11 @@ trait InstallsApiStack
 
         // Providers...
         $files->copyDirectory(__DIR__.'/../../stubs/api/App/Providers', app_path('Providers'));
+
+        // Routes...
+        copy(__DIR__.'/../../stubs/api/routes/api.php', base_path('routes/api.php'));
+        copy(__DIR__.'/../../stubs/api/routes/web.php', base_path('routes/web.php'));
+        copy(__DIR__.'/../../stubs/api/routes/auth.php', base_path('routes/auth.php'));
 
         // Configuration...
         $files->copyDirectory(__DIR__.'/../../stubs/api/config', config_path());
@@ -40,16 +50,6 @@ trait InstallsApiStack
             "'url' => env('APP_URL', 'http://localhost'),".PHP_EOL.PHP_EOL."    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000')",
             config_path('app.php')
         );
-
-        // Tests...
-        $this->installTests();
-
-        $files->delete(base_path('tests/Feature/Auth/PasswordConfirmationTest.php'));
-
-        // Routes...
-        copy(__DIR__.'/../../stubs/api/routes/api.php', base_path('routes/api.php'));
-        copy(__DIR__.'/../../stubs/api/routes/web.php', base_path('routes/web.php'));
-        copy(__DIR__.'/../../stubs/api/routes/auth.php', base_path('routes/auth.php'));
 
         // Environment...
         if (! $files->exists(base_path('.env'))) {
@@ -62,6 +62,12 @@ trait InstallsApiStack
             base_path('.env')
         );
 
+        // Tests...
+        $this->installTests();
+
+        $files->delete(base_path('tests/Feature/Auth/PasswordConfirmationTest.php'));
+
+        // Cleaning...
         $this->removeScaffoldingUnnecessaryForApis();
 
         $this->info('Breeze scaffolding installed successfully.');
