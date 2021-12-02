61ffbd4a4f2f0e055f0d2b0bf80926c7c904b2df
Taylor Otwell <taylor@laravel.com>
2021-12-02 07:15:36 -0600

    Breeze "API" / SPA Stack (#109)
    This adds a new api "stack" to Breeze. This stack removes all JS and CSS related scaffolding and installs controllers that properly handle the API authentication (powered by Sanctum) that would typically be required when authenticating a JavaScript SPA such as applications powered by Next.js / Nuxt.js, etc.

---
+++ src/Console/InstallsApiStack.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Laravel\Breeze\Console;
+
+use Illuminate\Filesystem\Filesystem;
+
+trait InstallsApiStack
+{
+    /**
+     * Install the API Breeze stack.
+     *
+     * @return void
+     */
+    protected function installApiStack()
+    {
+        $files = new Filesystem;
+
+        // Controllers...
+        $files->ensureDirectoryExists(app_path('Http/Controllers/Auth'));
+        $files->copyDirectory(__DIR__.'/../../stubs/api/App/Http/Controllers/Auth', app_path('Http/Controllers/Auth'));
+
+        // Middleware...
+        $files->copyDirectory(__DIR__.'/../../stubs/api/App/Http/Middleware', app_path('Http/Middleware'));
+
+        $this->replaceInFile('// \Laravel\Sanctum\Http', '\Laravel\Sanctum\Http', app_path('Http/Kernel.php'));
+
+        $this->replaceInFile(
+            '\Illuminate\Auth\Middleware\EnsureEmailIsVerified::class',
+            '\App\Http\Middleware\EnsureEmailIsVerified::class',
+            app_path('Http/Kernel.php')
+        );
+
+        // Requests...
+        $files->ensureDirectoryExists(app_path('Http/Requests/Auth'));
+        $files->copyDirectory(__DIR__.'/../../stubs/api/App/Http/Requests/Auth', app_path('Http/Requests/Auth'));
+
+        // Providers...
+        $files->copyDirectory(__DIR__.'/../../stubs/api/App/Providers', app_path('Providers'));
+
+        // Routes...
+        copy(__DIR__.'/../../stubs/api/routes/api.php', base_path('routes/api.php'));
+        copy(__DIR__.'/../../stubs/api/routes/web.php', base_path('routes/web.php'));
+        copy(__DIR__.'/../../stubs/api/routes/auth.php', base_path('routes/auth.php'));
+
+        // Configuration...
+        $files->copyDirectory(__DIR__.'/../../stubs/api/config', config_path());
+
+        $this->replaceInFile(
+            "'url' => env('APP_URL', 'http://localhost')",
+            "'url' => env('APP_URL', 'http://localhost'),".PHP_EOL.PHP_EOL."    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000')",
+            config_path('app.php')
+        );
+
+        // Environment...
+        if (! $files->exists(base_path('.env'))) {
+            copy(base_path('.env.example'), base_path('.env'));
+        }
+
+        $this->replaceInFile(
+            'APP_URL=http://localhost',
+            'APP_URL=http://localhost'.PHP_EOL.'FRONTEND_URL=http://localhost:3000',
+            base_path('.env')
+        );
+
+        // Tests...
+        $this->installTests();
+
+        $files->delete(base_path('tests/Feature/Auth/PasswordConfirmationTest.php'));
+
+        // Cleaning...
+        $this->removeScaffoldingUnnecessaryForApis();
+
+        $this->info('Breeze scaffolding installed successfully.');
+    }
+
+    /**
+     * Remove any application scaffolding that isn't needed for APIs.
+     *
+     * @return void
+     */
+    protected function removeScaffoldingUnnecessaryForApis()
+    {
+        $files = new Filesystem;
+
+        // Remove frontend related files...
+        $files->delete(base_path('package.json'));
+        $files->delete(base_path('webpack.mix.js'));
+
+        // Remove Laravel "welcome" view...
+        $files->delete(resource_path('views/welcome.blade.php'));
+        $files->put(resource_path('views/.gitkeep'), PHP_EOL);
+
+        // Remove CSS and JavaScript directories...
+        $files->deleteDirectory(resource_path('css'));
+        $files->deleteDirectory(resource_path('js'));
+    }
+}
---
+++ src/Console/InstallsBladeStack.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Laravel\Breeze\Console;
+
+use Illuminate\Filesystem\Filesystem;
+
+trait InstallsBladeStack
+{
+    /**
+     * Install the Blade Breeze stack.
+     *
+     * @return void
+     */
+    protected function installBladeStack()
+    {
+        // NPM Packages...
+        $this->updateNodePackages(function ($packages) {
+            return [
+                '@tailwindcss/forms' => '^0.2.1',
+                'alpinejs' => '^3.4.2',
+                'autoprefixer' => '^10.1.0',
+                'postcss' => '^8.2.1',
+                'postcss-import' => '^12.0.1',
+                'tailwindcss' => '^2.0.2',
+            ] + $packages;
+        });
+
+        // Controllers...
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers/Auth'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/App/Http/Controllers/Auth', app_path('Http/Controllers/Auth'));
+
+        // Requests...
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests/Auth'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/App/Http/Requests/Auth', app_path('Http/Requests/Auth'));
+
+        // Views...
+        (new Filesystem)->ensureDirectoryExists(resource_path('views/auth'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('views/layouts'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('views/components'));
+
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/resources/views/auth', resource_path('views/auth'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/resources/views/layouts', resource_path('views/layouts'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/resources/views/components', resource_path('views/components'));
+
+        copy(__DIR__.'/../../stubs/default/resources/views/dashboard.blade.php', resource_path('views/dashboard.blade.php'));
+
+        // Components...
+        (new Filesystem)->ensureDirectoryExists(app_path('View/Components'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/App/View/Components', app_path('View/Components'));
+
+        // Tests...
+        $this->installTests();
+
+        // Routes...
+        copy(__DIR__.'/../../stubs/default/routes/web.php', base_path('routes/web.php'));
+        copy(__DIR__.'/../../stubs/default/routes/auth.php', base_path('routes/auth.php'));
+
+        // "Dashboard" Route...
+        $this->replaceInFile('/home', '/dashboard', resource_path('views/welcome.blade.php'));
+        $this->replaceInFile('Home', 'Dashboard', resource_path('views/welcome.blade.php'));
+        $this->replaceInFile('/home', '/dashboard', app_path('Providers/RouteServiceProvider.php'));
+
+        // Tailwind / Webpack...
+        copy(__DIR__.'/../../stubs/default/tailwind.config.js', base_path('tailwind.config.js'));
+        copy(__DIR__.'/../../stubs/default/webpack.mix.js', base_path('webpack.mix.js'));
+        copy(__DIR__.'/../../stubs/default/resources/css/app.css', resource_path('css/app.css'));
+        copy(__DIR__.'/../../stubs/default/resources/js/app.js', resource_path('js/app.js'));
+
+        $this->info('Breeze scaffolding installed successfully.');
+        $this->comment('Please execute the "npm install && npm run dev" command to build your assets.');
+    }
+}
