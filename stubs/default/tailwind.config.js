8409c25fea42aeecb8e785d64725237e712e4b53
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2021-12-10 14:05:07 +0000

    Updates Tailwind on Installation to v3.0.0 (#110)
    * Change purge to content Tailwind v3.0.0
    
    * Changes flex-shrink to shrink Tailwind v3.0.0
    
    * Updates Tailwind on Installation to v3.0.0
    
    * Removed tailwind variants as no longer required

--- stubs/default/tailwind.config.js
+++ stubs/default/tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
+    content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -12,12 +12,6 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
-        },
-    },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
         },
     },
 
