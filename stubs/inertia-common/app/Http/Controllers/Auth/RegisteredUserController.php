627f2d0cb891282c7663f4c6495163f15c4a6b81
A Web Artisan <laraloop.com@gmail.com>
2021-12-23 16:32:09 +0100

    Fix Inertia Controllers @return tag and Inertia Vue Input Component (#112)
    * Fix correct @return tag for Inertia response
    
    * Fix Component/Input.vue autofocus
    
    * Update Input.vue
    
    * Update Input.vue
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- stubs/inertia-common/app/Http/Controllers/Auth/RegisteredUserController.php
+++ stubs/inertia-common/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -17,7 +17,7 @@ class RegisteredUserController extends Controller
     /**
      * Display the registration view.
      *
-     * @return \Illuminate\View\View
+     * @return \Inertia\Response
      */
     public function create()
     {
