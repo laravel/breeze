6e94ea5754d59e0ce0de8ef989f60cb06e618ff3
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-01-05 21:41:28 +0700

    Remove brackets inside react label component (#116)

--- stubs/inertia-react/resources/js/Components/Label.js
+++ stubs/inertia-react/resources/js/Components/Label.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Label({ forInput, value, className, children }) {
     return (
         <label htmlFor={forInput} className={`block font-medium text-sm text-gray-700 ` + className}>
-            {value ? value : { children }}
+            {value ? value : children}
         </label>
     );
 }
