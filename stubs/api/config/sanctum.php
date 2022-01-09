a21178ba157092e71c609105dc2b1758c445e75a
Kalle Pyörälä <kalle.pyorala@azurinspire.com>
2022-01-09 10:06:14 +0100

    Use FRONTEND_URL port in Sanctum config
    If `FRONTEND_URL` has some other port in local development than default :3000 it is dropped out here and cookies do not work. Other option would be change the [documentation](https://laravel.com/docs/8.x/starter-kits#breeze-and-next) to have a notice of including the host in SANCTUM_STATEFUL_DOMAINS.

--- stubs/api/config/sanctum.php
+++ stubs/api/config/sanctum.php
@@ -17,7 +17,12 @@ return [
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
+        env('FRONTEND_URL')
+            ? ','.implode(':', \Illuminate\Support\Arr::whereNotNull([
+                parse_url(env('FRONTEND_URL'), PHP_URL_HOST),
+                parse_url(env('FRONTEND_URL'), PHP_URL_PORT)
+            ]))
+            : ''
     ))),
 
     /*
