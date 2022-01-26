a082b7dd1883b55445072d8c4e19feb54acf362d
Muhammad Umair Raza <umairparacha0047@gmail.com>
2022-01-26 16:40:38 +0500

    FIX: Api stack EmailVerificationTest

--- stubs/api/tests/Feature/EmailVerificationTest.php
+++ stubs/api/tests/Feature/EmailVerificationTest.php
@@ -32,7 +32,7 @@ class EmailVerificationTest extends TestCase
 
         Event::assertDispatched(Verified::class);
         $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+        $response->assertRedirect(config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1');
     }
 
     public function test_email_is_not_verified_with_invalid_hash()
