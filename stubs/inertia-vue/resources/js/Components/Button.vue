1cc1b9589bce62cc81484bfa372ff5dc53980da5
Anjorin Damilare <damilareanjorin1@gmail.com>
2022-02-07 19:22:00 +0100

    add `props` validator and bind extra button attrs i.e disabled.

--- stubs/inertia-vue/resources/js/Components/Button.vue
+++ stubs/inertia-vue/resources/js/Components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" v-bind="$attrs" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
@@ -10,6 +10,7 @@ export default {
         type: {
             type: String,
             default: 'submit',
+            validator: (value) => ['button', 'submit'].includes(value)
         },
     }
 }
