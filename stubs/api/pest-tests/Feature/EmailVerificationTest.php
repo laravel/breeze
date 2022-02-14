9db4eb31734015a00a73c1c95783c036302f1043
Lukas Heller <dev@lukas-heller.de>
2022-02-14 16:42:36 +0100

    Update asserted redirect route

--- stubs/api/pest-tests/Feature/EmailVerificationTest.php
+++ stubs/api/pest-tests/Feature/EmailVerificationTest.php
@@ -23,7 +23,7 @@ test('email can be verified', function () {
 
     Event::assertDispatched(Verified::class);
     expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
-    $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+    $response->assertRedirect(config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1');
 });
 
 test('email is not verified with invalid hash', function () {
