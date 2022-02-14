774ac63ccc6bdac4e60305b1be9f95f271207224
Anjorin Damilare <damilareanjorin1@gmail.com>
2022-02-14 20:03:57 +0100

    Update auth.php

--- stubs/inertia-common/routes/auth.php
+++ stubs/inertia-common/routes/auth.php
@@ -36,7 +36,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    
+
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
