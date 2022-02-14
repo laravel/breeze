6eea2cbfa3c0a3da2da3bfff4d421e7bfc07fe9b
Anjorin Damilare <damilareanjorin1@gmail.com>
2022-02-14 20:05:08 +0100

    fix styleci

--- stubs/inertia-common/routes/auth.php
+++ stubs/inertia-common/routes/auth.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
@@ -36,7 +35,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
 
