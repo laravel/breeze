3451166ad02310ea0f82059910ebce0fced2f422
Shakil Ahmmed <shakilfci461@gmail.com>
2022-02-18 16:45:42 +0600

    Append Breeze Routes Into web.php after running install command

--- stubs/api/routes/web.php
+++ stubs/api/routes/web.php
@@ -1,18 +1,16 @@
-
-
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| AUTH Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Laravel Breeze Generated AUTH routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
--- stubs/inertia-common/routes/web.php
+++ stubs/inertia-common/routes/web.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Inertia AUTH Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Laravel Breeze Generated Inertia AUTH routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
