5bb65193c2e4e9ae7e63ad438ccbef13787352ff
Alpha Olomi <alphaolomi@gmail.com>
2022-02-19 21:14:49 +0300

    feat: add routes name to register and login  paths

--- stubs/api/routes/auth.php
+++ stubs/api/routes/auth.php
@@ -9,10 +9,12 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')
+                ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')
+                ->name('login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
                 ->middleware('guest')
