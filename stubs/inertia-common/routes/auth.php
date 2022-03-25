5fbdc9e71725a247406ecbf94dcb4a3b5d17e216
DerianCordobaPerez <derianunanleon@gmail.com>
2022-03-25 17:24:58 -0600

    refactor(Root): Grouping of the routes within their respective controllers

--- stubs/inertia-common/routes/auth.php
+++ stubs/inertia-common/routes/auth.php
@@ -11,27 +11,36 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::controller(RegisteredUserController::class)->group(function() {
+        Route::get('register', 'create')
+            ->name('register');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+        Route::post('register', 'store');
+    });
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::controller(AuthenticatedSessionController::class)->group(function() {
+        Route::get('login', 'create')
+            ->name('login');
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name('password.request');
+        Route::post('login', 'store');
+    });
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name('password.email');
+    Route::controller(PasswordResetLinkController::class)->group(function() {
+        Route::get('forgot-password', 'create')
+            ->name('password.request');
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name('password.reset');
+        Route::post('forgot-password', 'store')
+            ->name('password.email');
+    });
 
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name('password.update');
+    Route::controller(NewPasswordController::class)->group(function() {
+        Route::get('reset-password/{token}', 'create')
+            ->name('password.reset');
+
+        Route::post('reset-password', 'store')
+            ->name('password.update');
+    });
 });
 
 Route::middleware('auth')->group(function () {
@@ -46,10 +55,12 @@ Route::middleware('auth')->group(function () {
                 ->middleware('throttle:6,1')
                 ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
+    Route::controller(ConfirmablePasswordController::class)->group(function() {
+        Route::get('confirm-password', 'show')
+            ->name('password.confirm');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+        Route::post('confirm-password', 'store');
+    });
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
