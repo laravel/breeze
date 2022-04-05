a4c147e5901a2479203fb612d62afb605a66cf39
Ruslan Popelyshyn <platonpopelishin@gmail.com>
2022-04-05 21:07:50 +0300

    update inertia stub
    - removed useless lines

--- stubs/inertia-common/resources/views/app.blade.php
+++ stubs/inertia-common/resources/views/app.blade.php
@@ -19,9 +19,5 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
-
-        @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
-        @endenv
     </body>
 </html>
