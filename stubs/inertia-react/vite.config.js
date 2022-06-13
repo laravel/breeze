5c2088b51b62bc9f3510131e4ec69f9c8ff926ff
Jess Archer <jess@jessarcher.com>
2022-06-13 21:38:25 +1000

    Style fixes

--- stubs/inertia-react/vite.config.js
+++ stubs/inertia-react/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -9,4 +9,4 @@ export default defineConfig({
         }),
         react(),
     ],
-})
+});
