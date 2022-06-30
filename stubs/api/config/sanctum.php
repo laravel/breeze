3207d68b88a4d14a1aedde28f8b40310f5e8cc14
AkrAm <akram@futurestation.net>
2022-06-30 15:43:48 +0600

    imports added

--- stubs/api/config/sanctum.php
+++ stubs/api/config/sanctum.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Sanctum\Sanctum;
+
 return [
 
     /*
@@ -18,7 +20,7 @@ return [
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : '',
-        \Laravel\Sanctum\Sanctum::currentApplicationUrlWithPort()
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
