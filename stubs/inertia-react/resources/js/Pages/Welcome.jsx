a6817a45c9f49e2bf342c4a38e27a610cbda787a
Jess Archer <jess@jessarcher.com>
2022-08-08 23:25:54 +1000

    [1.x] Style improvements (#177)
    * Remove unnecessary styles from Vue welcome page
    
    * Add missing dark variant for welcome nav links
    
    * Fix underlined trailing white space in Vue welcome page
    
    * Make StyleCI happy

--- stubs/inertia-react/resources/js/Pages/Welcome.jsx
+++ stubs/inertia-react/resources/js/Pages/Welcome.jsx
@@ -8,16 +8,19 @@ export default function Welcome(props) {
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="fixed top-0 right-0 px-6 py-4 sm:block">
                     {props.auth.user ? (
-                        <Link href={route('dashboard')} className="text-sm text-gray-700 underline">
+                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
                             Dashboard
                         </Link>
                     ) : (
                         <>
-                            <Link href={route('login')} className="text-sm text-gray-700 underline">
+                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
                                 Log in
                             </Link>
 
-                            <Link href={route('register')} className="ml-4 text-sm text-gray-700 underline">
+                            <Link
+                                href={route('register')}
+                                className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
+                            >
                                 Register
                             </Link>
                         </>
