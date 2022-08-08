589be0d13a07877eeaa5208c4548819b400251c2
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-08-08 03:50:13 +0100

    chore: formatting

--- stubs/api/routes/auth.php
+++ stubs/api/routes/auth.php
@@ -9,17 +9,13 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::post('/register', [RegisteredUserController::class])
-        ->name('register');
+    Route::post('/register', [RegisteredUserController::class])->name('register');
 
-    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-        ->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
 
-    Route::post('/forgot-password', [PasswordResetLinkController::class])
-        ->name('password.email');
+    Route::post('/forgot-password', [PasswordResetLinkController::class])->name('password.email');
 
-    Route::post('/reset-password', [NewPasswordController::class])
-        ->name('password.update');
+    Route::post('/reset-password', [NewPasswordController::class])->name('password.update');
 });
 
 Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class])
