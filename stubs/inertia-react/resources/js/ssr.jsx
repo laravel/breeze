c3f7ec5680c1acbe40b1aa94abdef20056a1711b
Tim MacDonald <hello@timacdonald.me>
2022-09-06 17:12:48 +1000

    no need to eager glob ssr

--- stubs/inertia-react/resources/js/ssr.jsx
+++ stubs/inertia-react/resources/js/ssr.jsx
@@ -12,7 +12,7 @@ createServer((page) =>
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx', { eager: true })),
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
         setup: ({ App, props }) => {
             global.route = (name, params, absolute) =>
                 route(name, params, absolute, {
