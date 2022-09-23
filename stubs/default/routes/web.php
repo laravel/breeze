bf6886cbcca38b53edafe88cb1474afb2fbfb5e0
Jess Archer <jess@jessarcher.com>
2022-09-23 11:09:01 +1000

    Add verified middleware to Blade dashboard

--- stubs/default/routes/web.php
+++ stubs/default/routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
