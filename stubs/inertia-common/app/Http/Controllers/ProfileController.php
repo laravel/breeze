68ed0dd2b70f0eb67f6306f935023f0f0dfb9a69
Taylor Otwell <taylor@laravel.com>
2022-11-04 16:22:58 -0500

    Update ProfileController.php

--- stubs/inertia-common/app/Http/Controllers/ProfileController.php
+++ stubs/inertia-common/app/Http/Controllers/ProfileController.php
@@ -62,8 +62,11 @@ class ProfileController extends Controller
         ]);
 
         $user = $request->user();
+
         Auth::logout();
+
         $user->delete();
+
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
