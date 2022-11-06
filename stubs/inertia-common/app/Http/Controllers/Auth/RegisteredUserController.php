4bb8e353790b3d797dec4451262d2a389e9c7461
Viliam Dias <viliamjr@gmail.com>
2022-11-06 13:31:46 -0300

    RegisteredUserController stubs should use the User model name instead of table name in the validators (#206)
    * Class name instead of table name in validators:
    
    Validators in RegisteredUserController stubs
    should reference the User class name instead of
    table name to avoid  error throw when registering
    a new user after change the database schema
    (postgres) from the 'users' table.
    
    * Fix code style: suggested by StyleCI.

--- stubs/inertia-common/app/Http/Controllers/Auth/RegisteredUserController.php
+++ stubs/inertia-common/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,7 +36,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
