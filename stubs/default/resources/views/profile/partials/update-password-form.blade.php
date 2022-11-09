1745464184258b30f79b8e4c07415fc24a2a1557
Jess Archer <jess@jessarcher.com>
2022-11-09 16:27:40 +1000

    Display profile saved notification for Blade stack

--- stubs/default/resources/views/profile/partials/update-password-form.blade.php
+++ stubs/default/resources/views/profile/partials/update-password-form.blade.php
@@ -28,6 +28,15 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
+            @if (session('status') === 'password-updated')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600"
+                >Saved.</p>
+            @endif
         </div>
     </form>
 </section>
--- stubs/default/resources/views/profile/partials/update-profile-information-form.blade.php
+++ stubs/default/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -43,6 +43,15 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
+            @if (session('status') === 'profile-updated')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600"
+                >Saved.</p>
+            @endif
         </div>
     </form>
 </section>
