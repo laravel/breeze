a02433e1e3d9f7e9e16bff9340da7acbb56b172a
Jess Archer <jess@jessarcher.com>
2022-11-14 22:39:05 +1000

    Formatting

--- stubs/inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
+++ stubs/inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -99,7 +99,9 @@ export default function Authenticated({ auth, header, children }) {
 
                     <div className="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                         <div className="px-4">
-                            <div className="font-medium text-base text-gray-800 dark:text-gray-200">{auth.user.name}</div>
+                            <div className="font-medium text-base text-gray-800 dark:text-gray-200">
+                                {auth.user.name}
+                            </div>
                             <div className="font-medium text-sm text-gray-500">{auth.user.email}</div>
                         </div>
 
