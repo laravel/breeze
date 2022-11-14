c52aae31b4e69d2ec99362cf67c38341d648eed9
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-11-14 16:46:06 +0700

    Update web.php
    Implement route controller for `ProfileController`.

--- stubs/default/routes/web.php
+++ stubs/default/routes/web.php
@@ -22,10 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(ProfileController::class)->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', 'edit')->name('profile.edit');
+    Route::patch('/profile', 'update')->name('profile.update');
+    Route::delete('/profile', 'destroy')->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
