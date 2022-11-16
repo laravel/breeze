60ec32e0b7cedf11f71ff143410a9e47bc6915e7
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-16 20:36:31 +0100

    Splade version bump

--- stubs/splade/routes/web.php
+++ stubs/splade/routes/web.php
@@ -17,6 +17,9 @@ Route::middleware('splade')->group(function () {
     // Registers routes to support Table Bulk Actions and Exports...
     Route::spladeTable();
 
+    // Registers routes to support async File Uploads with Filepond...
+    Route::spladeUploads();
+
     Route::get('/', function () {
         return view('welcome');
     });
