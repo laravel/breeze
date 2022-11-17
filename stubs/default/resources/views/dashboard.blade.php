f487c87534bbdc58ff7a5ed7f260a24736ad9602
Jess Archer <jess@jessarcher.com>
2022-11-18 01:12:07 +1000

    Add missing translation methods to Blade stack (#215)
    * Add missing translation methods to Blade stack
    
    * formatting
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- stubs/default/resources/views/dashboard.blade.php
+++ stubs/default/resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    You're logged in!
+                    {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
