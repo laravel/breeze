c2859b43a678fbb1cc045b3fe03b417b72089484
Taylor Otwell <taylor@laravel.com>
2022-11-17 09:06:17 -0600

    formatting

--- stubs/default/resources/views/profile/partials/delete-user-form.blade.php
+++ stubs/default/resources/views/profile/partials/delete-user-form.blade.php
@@ -3,6 +3,7 @@
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Delete Account') }}
         </h2>
+
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
         </p>
