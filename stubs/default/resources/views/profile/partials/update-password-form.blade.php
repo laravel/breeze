20a863f32f427d614e7dfd0a562e8bd8af01b5ca
Taylor Otwell <taylor@laravel.com>
2022-11-17 09:05:40 -0600

    formatting

--- stubs/default/resources/views/profile/partials/update-password-form.blade.php
+++ stubs/default/resources/views/profile/partials/update-password-form.blade.php
@@ -3,6 +3,7 @@
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Update Password') }}
         </h2>
+
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
--- stubs/default/resources/views/profile/partials/update-profile-information-form.blade.php
+++ stubs/default/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -3,8 +3,9 @@
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Profile Information') }}
         </h2>
+
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Update your account\'s profile information and email address.') }}
+            {{ __("Update your account's profile information and email address.") }}
         </p>
     </header>
 
