701f16b86c17512ddada45ae2c57d43e22006fef
Florian <fouteox@gmail.com>
2022-11-24 17:37:02 +0100

    fix label

--- stubs/inertia-react/resources/js/Components/TextInput.jsx
+++ stubs/inertia-react/resources/js/Components/TextInput.jsx
@@ -17,6 +17,7 @@ export default forwardRef(function TextInput(
             <input
                 type={type}
                 name={name}
+                id={name}
                 value={value}
                 className={
                     `border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ` +
