de3bd3f1f2fce1e5584df4f88c45053afa3e9ff8
SENTQ <m@momagdi.com>
2022-12-09 00:37:37 +0200

    move duplicated logo component to guest layout

--- stubs/default/resources/views/auth/confirm-password.blade.php
+++ stubs/default/resources/views/auth/confirm-password.blade.php
@@ -1,11 +1,5 @@
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
         </div>
--- stubs/default/resources/views/auth/register.blade.php
+++ stubs/default/resources/views/auth/register.blade.php
@@ -1,11 +1,5 @@
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
