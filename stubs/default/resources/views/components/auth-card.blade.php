89bc1f58359c22d06360c898756f524e63db66e7
Taylor Otwell <taylor@laravel.com>
2022-12-09 12:56:07 -0600

    remove auth card

--- stubs/default/resources/views/components/auth-card.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="w-full px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-    {{ $slot }}
-</div>
--- stubs/default/resources/views/layouts/guest.blade.php
+++ stubs/default/resources/views/layouts/guest.blade.php
@@ -21,7 +21,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
