a0a4f424feac68fa1f7be504ed6bf4a3a99af5b1
Brian <brian@dillingham.dev>
2022-12-16 13:37:15 -0500

    Remove unused vue imports (#231)
    * Remove unused usePage
    
    * Remove unused ref

--- stubs/inertia-vue/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ stubs/inertia-vue/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -5,7 +5,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import Modal from '@/Components/Modal.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { useForm, usePage } from '@inertiajs/inertia-vue3';
+import { useForm } from '@inertiajs/inertia-vue3';
 import { nextTick, ref } from 'vue';
 
 const confirmingUserDeletion = ref(false);
--- stubs/inertia-vue/resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ stubs/inertia-vue/resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -4,7 +4,6 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Link, useForm, usePage } from '@inertiajs/inertia-vue3';
-import { ref } from 'vue';
 
 const props = defineProps({
     mustVerifyEmail: Boolean,
