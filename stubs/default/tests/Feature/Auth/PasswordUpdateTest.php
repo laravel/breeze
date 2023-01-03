b215bc2c1d5ee620101217d40348f77098befcc7
Nuno Maduro <enunomaduro@gmail.com>
2023-01-03 14:03:48 +0000

    Adds types to profile page related features

--- stubs/default/tests/Feature/Auth/PasswordUpdateTest.php
+++ stubs/default/tests/Feature/Auth/PasswordUpdateTest.php
@@ -11,7 +11,7 @@ class PasswordUpdateTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_password_can_be_updated()
+    public function test_password_can_be_updated(): void
     {
         $user = User::factory()->create();
 
@@ -31,7 +31,7 @@ class PasswordUpdateTest extends TestCase
         $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
 
-    public function test_correct_password_must_be_provided_to_update_password()
+    public function test_correct_password_must_be_provided_to_update_password(): void
     {
         $user = User::factory()->create();
 
