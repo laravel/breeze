ee1b89a61fb230f011b436520a9de9476a0e521d
Jess Archer <jess@jessarcher.com>
2023-01-13 16:36:58 +1000

    Update progress usage

--- stubs/inertia-react/resources/js/app.jsx
+++ stubs/inertia-react/resources/js/app.jsx
@@ -3,7 +3,6 @@ import '../css/app.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
-import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -16,6 +15,7 @@ createInertiaApp({
 
         root.render(<App {...props} />);
     },
+    progress: {
+        color: '#4B5563',
+    },
 });
-
-InertiaProgress.init({ color: '#4B5563' });
