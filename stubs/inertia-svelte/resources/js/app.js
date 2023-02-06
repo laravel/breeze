3d20d303e101b57de6ab541789d794409b05c0fd
1CAMOU <mgmt.camou@gmail.com>
2023-02-06 22:23:25 +0100

    fix: StyleCI

--- stubs/inertia-svelte/resources/js/app.js
+++ stubs/inertia-svelte/resources/js/app.js
@@ -1,15 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createInertiaApp } from '@inertiajs/svelte'
+import { createInertiaApp } from '@inertiajs/svelte';
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
-        return pages[`./Pages/${name}.svelte`]
+    resolve: (name) => {
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
+        return pages[`./Pages/${name}.svelte`];
     },
     setup({ el, App, props }) {
-        new App({ target: el, props, })
+        new App({ target: el, props, });
     },
     progress: {
         color: '#4B5563',
