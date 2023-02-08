a5bf9e0f468418a80c7a772ff4f9c654f1ae24b4
Jess Archer <jess@jessarcher.com>
2023-02-08 14:55:21 +1000

    Update font and welcome page

--- stubs/default/tailwind.config.js
+++ stubs/default/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
