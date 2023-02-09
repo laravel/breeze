a7d94cb562f158442a34bc31c62564fa302d344c
Nuno Maduro <enunomaduro@gmail.com>
2023-02-09 14:42:10 +0000

    adjusts default argument

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -157,7 +157,7 @@ class InstallCommand extends Command
      * @param  bool  $asDev
      * @return bool
      */
-    protected function removeComposerPackages($packages, $asDev)
+    protected function removeComposerPackages($packages, $asDev = false)
     {
         $composer = $this->option('composer');
 
@@ -185,7 +185,7 @@ class InstallCommand extends Command
      * @param  bool  $asDev
      * @return bool
      */
-    protected function requireComposerPackages($packages, $asDev)
+    protected function requireComposerPackages($packages, $asDev = false)
     {
         $composer = $this->option('composer');
 
