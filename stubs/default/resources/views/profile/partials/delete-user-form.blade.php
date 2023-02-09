cab0de9e44343e7cfe04bddc5029f7e749188c41
castrohenworx <castro@henworx.de>
2023-02-09 12:30:21 +0100

    Add translation to placeholder in delete-user-form.blade.php

--- stubs/default/resources/views/profile/partials/delete-user-form.blade.php
+++ stubs/default/resources/views/profile/partials/delete-user-form.blade.php
@@ -35,7 +35,7 @@
                     name="password"
                     type="password"
                     class="mt-1 block w-3/4"
-                    placeholder="Password"
+                    placeholder="{{ __('Password') }}"
                 />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
