1eee5144dbaacb9287405ac1512821b68b4dec30
Paulo Castellano <paulofcastellano@gmail.com>
2023-02-15 09:59:50 -0300

    Update app.blade.php (inertia-vue)
    The inertia docs https://inertiajs.com/server-side-setup, recommend uses @Vite('resources/js/app.js').
    
    The old way creates 502 errors because cannot render complex components on the build.

--- stubs/inertia-vue/resources/views/app.blade.php
+++ stubs/inertia-vue/resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite('resources/js/app.js')
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
