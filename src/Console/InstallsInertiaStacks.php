7e75ec02d3f8609724636fd36b85dfba2886ae3f
Mr. Alpaca <jens.vdwiel@gmail.com>
2023-02-17 14:37:00 +0100

    created vue with typescript install functions

--- src/Console/InstallsInertiaStacks.php
+++ src/Console/InstallsInertiaStacks.php
@@ -107,7 +107,109 @@ trait InstallsInertiaStacks
         $this->line('');
         $this->components->info('Breeze scaffolding installed successfully.');
     }
+    /**
+     * Install the Inertia Vue Typescript Breeze stack.
+     *
+     * @return int|null
+     */
+    protected function installInertiaVueTypescriptStack()
+    {
+        // Install Inertia...
+        if (! $this->requireComposerPackages(['inertiajs/inertia-laravel:^0.6.8', 'laravel/sanctum:^3.2', 'tightenco/ziggy:^1.0'])) {
+            return 1;
+        }
 
+        // NPM Packages...
+        $this->updateNodePackages(function ($packages) {
+            return [
+                    '@inertiajs/vue3' => '^1.0.0',
+                    '@tailwindcss/forms' => '^0.5.3',
+                    '@vitejs/plugin-vue' => '^4.0.0',
+                    'autoprefixer' => '^10.4.12',
+                    'postcss' => '^8.4.18',
+                    'tailwindcss' => '^3.2.1',
+                    'vue' => '^3.2.41',
+                    "typescript"=> "4.9.5",
+                ] + $packages;
+        });
+
+        // Controllers...
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/app/Http/Controllers', app_path('Http/Controllers'));
+
+        // Requests...
+        (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/default/app/Http/Requests', app_path('Http/Requests'));
+
+        // Middleware...
+        $this->installMiddlewareAfter('SubstituteBindings::class', '\App\Http\Middleware\HandleInertiaRequests::class');
+        $this->installMiddlewareAfter('\App\Http\Middleware\HandleInertiaRequests::class', '\Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class');
+
+        copy(__DIR__.'/../../stubs/inertia-common/app/Http/Middleware/HandleInertiaRequests.php', app_path('Http/Middleware/HandleInertiaRequests.php'));
+
+        // Views...
+        copy(__DIR__.'/../../stubs/inertia-vue-ts/resources/views/app.blade.php', resource_path('views/app.blade.php'));
+
+        // Components + Pages...
+        (new Filesystem)->ensureDirectoryExists(resource_path('js/Components'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('js/Layouts'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('js/Pages'));
+
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-vue-ts/resources/js/Components', resource_path('js/Components'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-vue-ts/resources/js/Layouts', resource_path('js/Layouts'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-vue-ts/resources/js/Pages', resource_path('js/Pages'));
+
+        if (! $this->option('dark')) {
+            $this->removeDarkClasses((new Finder)
+                ->in(resource_path('js'))
+                ->name('*.vue')
+                ->notName('Welcome.vue')
+            );
+        }
+
+        // Tests...
+        if (! $this->installTests()) {
+            return 1;
+        }
+
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/tests/Feature', base_path('tests/Feature'));
+
+        // Routes...
+        copy(__DIR__.'/../../stubs/inertia-common/routes/web.php', base_path('routes/web.php'));
+        copy(__DIR__.'/../../stubs/inertia-common/routes/auth.php', base_path('routes/auth.php'));
+
+        // "Dashboard" Route...
+        $this->replaceInFile('/home', '/dashboard', resource_path('js/Pages/Welcome.vue'));
+        $this->replaceInFile('Home', 'Dashboard', resource_path('js/Pages/Welcome.vue'));
+        $this->replaceInFile('/home', '/dashboard', app_path('Providers/RouteServiceProvider.php'));
+
+
+        // Tailwind / Vite...
+        copy(__DIR__.'/../../stubs/default/resources/css/app.css', resource_path('css/app.css'));
+        copy(__DIR__.'/../../stubs/default/postcss.config.js', base_path('postcss.config.js'));
+        copy(__DIR__.'/../../stubs/inertia-common/tailwind.config.js', base_path('tailwind.config.js'));
+        copy(__DIR__.'/../../stubs/inertia-common/jsconfig.json', base_path('jsconfig.json'));
+        copy(__DIR__.'/../../stubs/inertia-vue-ts/vite.config.js', base_path('vite.config.js'));
+        copy(__DIR__.'/../../stubs/inertia-vue-ts/resources/js/app.ts', resource_path('js/app.ts'));
+        copy(__DIR__. '/../../stubs/inertia-vue-ts/resources/js/tsconfig.json', base_path('tsconfig.json'));
+
+        if ($this->option('ssr')) {
+            $this->installInertiaVueSsrStack();
+        }
+
+        $this->components->info('Installing and building Node dependencies.');
+
+        if (file_exists(base_path('pnpm-lock.yaml'))) {
+            $this->runCommands(['pnpm install', 'pnpm run build']);
+        } elseif (file_exists(base_path('yarn.lock'))) {
+            $this->runCommands(['yarn install', 'yarn run build']);
+        } else {
+            $this->runCommands(['npm install', 'npm run build']);
+        }
+
+        $this->line('');
+        $this->components->info('Breeze scaffolding installed successfully.');
+    }
     /**
      * Install the Inertia Vue SSR stack into the application.
      *
@@ -117,12 +219,30 @@ trait InstallsInertiaStacks
     {
         $this->updateNodePackages(function ($packages) {
             return [
-                '@vue/server-renderer' => '^3.2.31',
-            ] + $packages;
+                    '@vue/server-renderer' => '^3.2.31',
+                ] + $packages;
         });
 
         copy(__DIR__.'/../../stubs/inertia-vue/resources/js/ssr.js', resource_path('js/ssr.js'));
         $this->replaceInFile("input: 'resources/js/app.js',", "input: 'resources/js/app.js',".PHP_EOL."            ssr: 'resources/js/ssr.js',", base_path('vite.config.js'));
+        $this->replaceInFile('vite build', 'vite build && vite build --ssr', base_path('package.json'));
+        $this->replaceInFile('/node_modules', '/bootstrap/ssr'.PHP_EOL.'/node_modules', base_path('.gitignore'));
+    }
+    /**
+     * Install the Inertia Vue SSR stack into the application.
+     *
+     * @return void
+     */
+    protected function installInertiaVueSsrTypescriptStack()
+    {
+        $this->updateNodePackages(function ($packages) {
+            return [
+                '@vue/server-renderer' => '^3.2.31',
+            ] + $packages;
+        });
+
+        copy(__DIR__.'/../../stubs/inertia-vue-ts/resources/js/ssr.js', resource_path('js/ssr.ts'));
+        $this->replaceInFile("input: 'resources/js/app.ts',", "input: 'resources/js/app.ts',".PHP_EOL."            ssr: 'resources/js/ssr.ts',", base_path('vite.config.js'));
         $this->replaceInFile('vite build', 'vite build && vite build --ssr', base_path('package.json'));
         $this->replaceInFile('/node_modules', '/bootstrap/ssr'.PHP_EOL.'/node_modules', base_path('.gitignore'));
     }
