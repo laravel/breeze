9083d70a76a2f8e30a2a8b11e15e1792fae88b46
Taylor Otwell <taylor@laravel.com>
2023-03-08 11:19:29 -0600

    wip

--- stubs/default/pest-tests/Feature/ProfileTest.php
+++ stubs/default/pest-tests/Feature/ProfileTest.php
@@ -17,7 +17,7 @@ test('profile information can be updated', function () {
 
     $response = $this
         ->actingAs($user)
-        ->patch('/profile', [
+        ->put('/profile', [
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
@@ -38,7 +38,7 @@ test('email verification status is unchanged when the email address is unchanged
 
     $response = $this
         ->actingAs($user)
-        ->patch('/profile', [
+        ->put('/profile', [
             'name' => 'Test User',
             'email' => $user->email,
         ]);
