89d4ce2446e718e3609df0bdfc9246c61618ea7e
Jess Archer <jess@jessarcher.com>
2023-03-09 01:09:11 +1000

    Remove unnecessary text input wrapper (#270)

--- stubs/inertia-react/resources/js/Components/TextInput.jsx
+++ stubs/inertia-react/resources/js/Components/TextInput.jsx
@@ -10,16 +10,14 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
     }, []);
 
     return (
-        <div className="flex flex-col items-start">
-            <input
-                {...props}
-                type={type}
-                className={
-                    'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-                    className
-                }
-                ref={input}
-            />
-        </div>
+        <input
+            {...props}
+            type={type}
+            className={
+                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                className
+            }
+            ref={localRef}
+        />
     );
 });
