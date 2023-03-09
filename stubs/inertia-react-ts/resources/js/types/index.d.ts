92b2cf617a4abc84a6cc3b2d16f3cd8f83d59738
Jess Archer <jess@jessarcher.com>
2023-03-10 09:45:10 +1000

    Replace `{}` type with something safer

--- stubs/inertia-react-ts/resources/js/types/index.d.ts
+++ stubs/inertia-react-ts/resources/js/types/index.d.ts
@@ -5,7 +5,7 @@ export interface User {
     email_verified_at: string;
 }
 
-export type PageProps<T extends Record<string, unknown> = {}> = T & {
+export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
