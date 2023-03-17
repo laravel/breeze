3fa7f63dbb9ba9bff18b74f46c1d8c429be267b7
Jess Archer <jess@jessarcher.com>
2023-03-18 00:21:47 +1000

    Fix Vue Modal maxWidth prop type

--- stubs/inertia-vue-ts/resources/js/Components/Modal.vue
+++ stubs/inertia-vue-ts/resources/js/Components/Modal.vue
@@ -4,7 +4,7 @@ import { computed, onMounted, onUnmounted, watch } from 'vue';
 const props = withDefaults(
     defineProps<{
         show?: boolean;
-        maxWidth?: '2xl';
+        maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
         closeable?: boolean;
     }>(),
     {
