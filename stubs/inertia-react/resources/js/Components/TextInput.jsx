62c79d5d32962df5af2584ad92692a90eb69c10f
denis <super.denisko@gmail.com>
2023-03-21 02:27:47 -0400

    Fix ref in react TextInput (#277)
    Fix ref in react TextInput

--- stubs/inertia-react/resources/js/Components/TextInput.jsx
+++ stubs/inertia-react/resources/js/Components/TextInput.jsx
@@ -17,7 +17,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
                 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
                 className
             }
-            ref={localRef}
+            ref={input}
         />
     );
 });
