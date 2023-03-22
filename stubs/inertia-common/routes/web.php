02a3400cadc75bc4f5d55f09f451b6e81bc92ff0
Bhargav Raviya <46953122+bhargavraviya@users.noreply.github.com>
2023-03-22 13:04:01 +0530

    Update web.php
    change web.php using controller functionality

--- stubs/inertia-common/routes/web.php
+++ stubs/inertia-common/routes/web.php
@@ -29,10 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->controller(ProfileController::class)->group(function () {
+    Route::get('/profile', 'edit')->name('profile.edit');
+    Route::patch('/profile', 'update')->name('profile.update');
+    Route::delete('/profile', 'destroy')->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
