2f3282657dd0ee6efd9dc71a7d62fea98d2256c4
Guilherme Saade <saade@outlook.com.br>
2023-03-22 09:34:18 -0300

    Update `current_password` rule in ProfileController (#278)
    * update `current_password` rule
    
    * update `current_password` rule in inertia controller

--- stubs/inertia-common/app/Http/Controllers/ProfileController.php
+++ stubs/inertia-common/app/Http/Controllers/ProfileController.php
@@ -46,7 +46,7 @@ class ProfileController extends Controller
     public function destroy(Request $request): RedirectResponse
     {
         $request->validate([
-            'password' => ['required', 'current-password'],
+            'password' => ['required', 'current_password'],
         ]);
 
         $user = $request->user();
