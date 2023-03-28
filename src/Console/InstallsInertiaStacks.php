bb2935f40d8396c6d0c77e4099a6e072c9095b33
Jess Archer <jess@jessarcher.com>
2023-03-29 00:37:27 +1000

    [1.x] Fix Pest test installation for Inertia stack (#279)
    * Fix installation of Inertia-specific Pest tests
    
    * Add missing failure handler

--- src/Console/InstallsInertiaStacks.php
+++ src/Console/InstallsInertiaStacks.php
@@ -88,7 +88,11 @@ trait InstallsInertiaStacks
             return 1;
         }
 
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/tests/Feature', base_path('tests/Feature'));
+        if ($this->option('pest')) {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/pest-tests/Feature', base_path('tests/Feature'));
+        } else {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/tests/Feature', base_path('tests/Feature'));
+        }
 
         // Routes...
         copy(__DIR__.'/../../stubs/inertia-common/routes/web.php', base_path('routes/web.php'));
@@ -247,8 +251,15 @@ trait InstallsInertiaStacks
         }
 
         // Tests...
-        $this->installTests();
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/tests/Feature', base_path('tests/Feature'));
+        if (! $this->installTests()) {
+            return 1;
+        }
+
+        if ($this->option('pest')) {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/pest-tests/Feature', base_path('tests/Feature'));
+        } else {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia-common/tests/Feature', base_path('tests/Feature'));
+        }
 
         // Routes...
         copy(__DIR__.'/../../stubs/inertia-common/routes/web.php', base_path('routes/web.php'));
