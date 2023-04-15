020a126b1b32262f6af79121fe3b855c1f9b5e54
Nihal Maskey <26411488+maskeynihal@users.noreply.github.com>
2023-04-16 00:29:15 +0545

    Update stub of sanctum in breeze to match config file of sanctum
    1. Update the stub to match https://github.com/laravel/sanctum/blob/3.x/config/sanctum.php
    2. Allow `port` in the URL for `FRONTEND_URL`
    
    formatting fix
    
    style:fix

--- stubs/api/config/sanctum.php
+++ stubs/api/config/sanctum.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Sanctum\Sanctum;
+
 return [
 
     /*
@@ -16,9 +18,27 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
+        Sanctum::currentApplicationUrlWithPort(),
+        env('FRONTEND_URL')
+            ? ','.
+              parse_url(env('FRONTEND_URL'), PHP_URL_HOST).
+              (parse_url(env('FRONTEND_URL'), PHP_URL_PORT) ? ':'.parse_url(env('FRONTEND_URL'), PHP_URL_PORT) : '')
+            : ''
     ))),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Guards
+    |--------------------------------------------------------------------------
+    |
+    | This array contains the authentication guards that will be checked when
+    | Sanctum is trying to authenticate a request. If none of these guards
+    | are able to authenticate the request, Sanctum will use the bearer
+    | token that's present on an incoming request for authentication.
+    |
+    */
+
+    'guard' => ['web'],
 
     /*
     |--------------------------------------------------------------------------
@@ -26,8 +46,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value controls the number of minutes until an issued token will be
-    | considered expired. If this value is null, personal access tokens do
-    | not expire. This won't tweak the lifetime of first-party sessions.
+    | considered expired. This will override any values set in the token's
+    | "expires_at" attribute, but first-party sessions are not affected.
     |
     */
 
