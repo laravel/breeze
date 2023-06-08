02d8d5468bb613beb40d6fc6a557d8ea8baaa0ac
Mário Lucas <mariolucasdev@gmail.com>
2023-06-08 10:21:23 -0300

    fix return has no type specified in iterable type array

--- stubs/default/app/Http/Requests/Auth/LoginRequest.php
+++ stubs/default/app/Http/Requests/Auth/LoginRequest.php
@@ -22,7 +22,7 @@ class LoginRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array<string>|string>
      */
     public function rules(): array
     {
--- stubs/default/app/Http/Requests/ProfileUpdateRequest.php
+++ stubs/default/app/Http/Requests/ProfileUpdateRequest.php
@@ -11,7 +11,7 @@ class ProfileUpdateRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array<string>|string>
      */
     public function rules(): array
     {
