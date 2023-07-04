3bfc9fd82345070c55dc1317f67d819ac57b20bd
clem <clementm.mas@gmail.com>
2023-07-04 14:57:36 +0700

    auth controller remove duplicate regenerate session

--- stubs/default/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ stubs/default/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,8 +27,6 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
-
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
