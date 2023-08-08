96fe4962ac31c04a734a7d1a677831d62d75ef7b
Altin <softize@gmail.com>
2023-08-09 01:34:18 +0200

    Fix 404 error on password reset link received in emails
    Password reset link was wrong and it would leat to 404 error.
    
    the link sent to email was this: 
    http://localhost:3000/password-reset/{TOKEN}?email={EMAIL}
    
    and it should have been this:
    http://localhost:3000/reset-password/{TOKEN}?email={EMAIL}
    
    by using named route we make sure the correct link is sent and this problem doesn't happen again.

--- stubs/api/app/Providers/AuthServiceProvider.php
+++ stubs/api/app/Providers/AuthServiceProvider.php
@@ -24,7 +24,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return route('password.reset', ['token'=>$token]) . "?email={$notifiable->getEmailForPasswordReset()}";
         });
 
         //
