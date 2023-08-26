feac7d8d3922086ccb487a6d223d9c2020a639a6
Ostap Brehin <osbre@protonmail.com>
2023-08-26 17:39:50 +0100

    StyleCI

--- stubs/inertia-common/app/Http/Middleware/HandleInertiaRequests.php
+++ stubs/inertia-common/app/Http/Middleware/HandleInertiaRequests.php
@@ -35,7 +35,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
-            'ziggy' => fn() => [
+            'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
