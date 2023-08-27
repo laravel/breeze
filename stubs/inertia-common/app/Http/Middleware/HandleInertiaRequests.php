6d2c37a5d9febe9d7714ecf7c85db847591b3bb5
Ostap Brehin <osbre@protonmail.com>
2023-08-27 15:49:38 +0100

    [1.x] Replace `array_merge` with spread operator in `HandleInertiaRequests` (#302)
    * [1.x] Replace `array_merge` with spread operator in `HandleInertiaRequests`
    
    * StyleCI

--- stubs/inertia-common/app/Http/Middleware/HandleInertiaRequests.php
+++ stubs/inertia-common/app/Http/Middleware/HandleInertiaRequests.php
@@ -30,15 +30,15 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
+        return [
+            ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
             ],
-            'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
-                    'location' => $request->url(),
-                ]);
-            },
-        ]);
+            'ziggy' => fn () => [
+                ...(new Ziggy)->toArray(),
+                'location' => $request->url(),
+            ],
+        ];
     }
 }
