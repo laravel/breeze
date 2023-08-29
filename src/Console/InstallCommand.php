d4891900c799af65a76530336aac9102a2261621
Saeed Vaziry <61919774+saeedvaziry@users.noreply.github.com>
2023-08-29 20:22:13 +0200

    Add File facade

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\File;
 
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\multiselect;
