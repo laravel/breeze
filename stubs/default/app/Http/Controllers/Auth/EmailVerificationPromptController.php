6ece6561dcfc1c942ed2838109889223bc83443e
hdood <m.bouguerzi1234@gmail.com>
2023-09-05 23:43:56 +0100

    sending the verification email to the user before redirecting to the email verification prompt view

--- stubs/default/app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ stubs/default/app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,8 +15,12 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(RouteServiceProvider::HOME)
-                    : view('auth.verify-email');
+        if($request->user()->hasVerifiedEmail())
+        {
+            return redirect()->intended(RouteServiceProvider::HOME); 
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+        return view('auth.verify-email');
     }
 }
