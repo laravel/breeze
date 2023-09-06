4e7df3bb1346cd0136e1ea61d62c2ce8cf367975
Francisco Madeira <xico2k@gmail.com>
2023-09-06 16:51:20 +0100

    Add logout tests. (#311)

--- stubs/api/pest-tests/Feature/Auth/AuthenticationTest.php
+++ stubs/api/pest-tests/Feature/Auth/AuthenticationTest.php
@@ -24,3 +24,12 @@ test('users can not authenticate with invalid password', function () {
 
     $this->assertGuest();
 });
+
+test('users can logout', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post('/logout');
+
+    $this->assertGuest();
+    $response->assertNoContent();
+});
