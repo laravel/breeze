7b8a77744accdd45d745009777ec4e0bf85764ef
Nuno Maduro <enunomaduro@gmail.com>
2023-09-11 13:00:51 +0100

    Installs Livewire and Volt

--- src/Console/InstallsLivewireStack.php
+++ src/Console/InstallsLivewireStack.php
@@ -12,7 +12,10 @@ trait InstallsLivewireStack
      */
     protected function installLivewireStack($functional)
     {
-        // ...
+        // Install Livewire...
+        if (! $this->requireComposerPackages(['livewire/livewire:^3.0', 'livewire/volt:^1.0'])) {
+            return 1;
+        }
 
         $this->line('');
         $this->components->info('Livewire scaffolding installed successfully.');
