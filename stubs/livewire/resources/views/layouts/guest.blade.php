113463b96572ec1e382ff8923d9aa03a73103ade
Nuno Maduro <enunomaduro@gmail.com>
2023-09-13 13:46:57 +0100

    Adds wire:navigate to application logo

--- stubs/livewire/resources/views/layouts/guest.blade.php
+++ stubs/livewire/resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <a href="/">
+                <a href="/" wire:navigate>
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
