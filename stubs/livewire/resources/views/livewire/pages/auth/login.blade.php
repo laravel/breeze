762d88bd275d7ede96324e2a87b1eb8b6e4d5765
Nuno Maduro <enunomaduro@gmail.com>
2023-09-13 13:45:26 +0100

    Adds wire:navigate to reset password

--- stubs/livewire/resources/views/livewire/pages/auth/login.blade.php
+++ stubs/livewire/resources/views/livewire/pages/auth/login.blade.php
@@ -102,7 +102,7 @@ class extends Component
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
