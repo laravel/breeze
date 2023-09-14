fd1506dc7eea0dc579f2009ffadd665e5d19605b
Nuno Maduro <enunomaduro@gmail.com>
2023-09-14 15:03:03 +0100

    Adds update password

---
+++ stubs/livewire/resources/views/livewire/profile/update-password-form.blade.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Livewire\Volt\Component;
+
+new class extends Component
+{
+    public function updatePassword(): void
+    {
+        $validated = $this->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', Password::defaults(), 'confirmed'],
+        ]);
+
+        auth()->user()->update([
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $this->dispatch('password-updated');
+    }
+}; ?>
+
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Update Password') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        </p>
+    </header>
+
+    <form wire:submit="updatePassword" class="mt-6 space-y-6">
+
+        <div>
+            <x-input-label for="current_password" :value="__('Current Password')" />
+            <x-text-input wire:model="current_password" id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+        </div>
+
+        <div>
+            <x-input-label for="password" :value="__('New Password')" />
+            <x-text-input wire:model="password" id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+        </div>
+
+        <div>
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-text-input wire:model="password" id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+            <x-action-message class="mr-3" on="password-updated">
+                {{ __('Saved.') }}
+            </x-action-message>
+        </div>
+    </form>
+</section>
--- stubs/livewire/resources/views/profile.blade.php
+++ stubs/livewire/resources/views/profile.blade.php
@@ -15,7 +15,7 @@
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    //
+                    <livewire:profile.update-password-form />
                 </div>
             </div>
 
