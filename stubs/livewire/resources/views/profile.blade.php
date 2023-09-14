5f50948e2b738d580da51918de6d53ad0c2ba9cb
Nuno Maduro <enunomaduro@gmail.com>
2023-09-14 17:18:12 +0100

    Adds delete user form

--- stubs/livewire/resources/views/profile.blade.php
+++ stubs/livewire/resources/views/profile.blade.php
@@ -21,7 +21,7 @@
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    //
+                    <livewire:profile.delete-user-form />
                 </div>
             </div>
         </div>
