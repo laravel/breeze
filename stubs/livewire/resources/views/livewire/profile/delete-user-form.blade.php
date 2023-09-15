6b072b74495531b0c1fe47f1cf8fc66a7bb674c2
Nuno Maduro <enunomaduro@gmail.com>
2023-09-15 13:36:18 +0100

    Fixes delete account errors

--- stubs/livewire/resources/views/livewire/profile/delete-user-form.blade.php
+++ stubs/livewire/resources/views/livewire/profile/delete-user-form.blade.php
@@ -64,7 +64,7 @@ new class extends Component
                     placeholder="{{ __('Password') }}"
                 />
 
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">
