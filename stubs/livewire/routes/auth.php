884a7ea3b66b5556b0d33a9b740ec48bd5ae6ac3
Nuno Maduro <enunomaduro@gmail.com>
2023-09-18 18:19:35 +0100

    Ordering

--- stubs/livewire/routes/auth.php
+++ stubs/livewire/routes/auth.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
-    Volt::route('login', 'pages.auth.login')
-        ->name('login');
-
     Volt::route('register', 'pages.auth.register')
         ->name('register');
+
+    Volt::route('login', 'pages.auth.login')
+        ->name('login');
 
     Volt::route('forgot-password', 'pages.auth.forgot-password')
         ->name('password.request');
@@ -19,13 +19,13 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Volt::route('verify-email', 'pages.auth.verify-email')
+        ->name('verification.notice');
+
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
     Volt::route('confirm-password', 'pages.auth.confirm-password')
         ->name('password.confirm');
-
-    Volt::route('verify-email', 'pages.auth.verify-email')
-        ->name('verification.notice');
 });
