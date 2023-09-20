f31f46d709f35bf41e876ee69bae176dba2c0ccc
Nuno Maduro <enunomaduro@gmail.com>
2023-09-20 18:09:52 +0100

    Removes `logged-in` component

--- stubs/livewire/resources/views/dashboard.blade.php
+++ stubs/livewire/resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <livewire:dashboard.logged-in />
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
             </div>
         </div>
     </div>
