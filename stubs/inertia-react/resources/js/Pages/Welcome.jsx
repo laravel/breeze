0d04846ec9fd8ff8ce9d18898d13340d6a6f29b3
Mostafa Said <mostafasaid1994@gmail.com>
2023-09-24 18:29:39 +0300

    fix: :bug: bring navigation links on top of other elements

--- stubs/inertia-react/resources/js/Pages/Welcome.jsx
+++ stubs/inertia-react/resources/js/Pages/Welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <>
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                <div className="sm:fixed sm:top-0 sm:right-0 sm:z-10 p-6 text-right">
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
--- stubs/inertia-vue/resources/js/Pages/Welcome.vue
+++ stubs/inertia-vue/resources/js/Pages/Welcome.vue
@@ -25,7 +25,7 @@ defineProps({
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
     >
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 sm:z-10 p-6 text-right">
             <Link
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
