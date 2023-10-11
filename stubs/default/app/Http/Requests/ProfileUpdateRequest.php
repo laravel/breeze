257a79a86c20962b37ffaf98d769cfe6d998c2d5
Nuno Maduro <enunomaduro@gmail.com>
2023-10-11 15:25:32 +0100

    Adds `lowercase` validation rule to user's `email` field

--- stubs/default/app/Http/Requests/ProfileUpdateRequest.php
+++ stubs/default/app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,8 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
-            'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
 }
--- stubs/livewire/resources/views/livewire/profile/update-profile-information-form.blade.php
+++ stubs/livewire/resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -23,7 +23,7 @@ new class extends Component
 
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', Rule::unique(User::class)->ignore($user->id)],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($user->id)],
         ]);
 
         $user->fill($validated);
