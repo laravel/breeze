1bf59521b6788adbe1c94e7ad5c61a8a8abd8ac3
Johnny Walker <johnny@johnnywalkerdigital.com>
2023-11-16 17:05:45 +0000

    Update RegisteredUserController.php

--- stubs/default/app/Http/Controllers/Auth/RegisteredUserController.php
+++ stubs/default/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
