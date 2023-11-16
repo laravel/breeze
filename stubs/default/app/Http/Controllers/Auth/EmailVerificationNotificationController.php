e9f44ccd065966ac06291140d914a45bc1cd8eb9
Johnny Walker <johnny@johnnywalkerdigital.com>
2023-11-16 17:03:41 +0000

    Update EmailVerificationNotificationController.php

--- stubs/default/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ stubs/default/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
