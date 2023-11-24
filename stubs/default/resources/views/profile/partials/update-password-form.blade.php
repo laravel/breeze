59c7fbbe503c4b2281b736a6404bd02bf84836ba
Taylor Otwell <taylor@laravel.com>
2023-11-24 08:41:03 -0600

    Update update-password-form.blade.php

--- stubs/default/resources/views/profile/partials/update-password-form.blade.php
+++ stubs/default/resources/views/profile/partials/update-password-form.blade.php
@@ -26,8 +26,8 @@
         </div>
 
         <div>
-            <x-input-label for="update_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
+            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
