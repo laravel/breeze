319e70b637c274c3d17eaacaf90906f5d5d7889e
Jess Archer <jess@jessarcher.com>
2024-01-02 11:35:34 +1000

    Use `model` convention from docs

--- stubs/inertia-vue-ts/resources/js/Components/TextInput.vue
+++ stubs/inertia-vue-ts/resources/js/Components/TextInput.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-const modelValue = defineModel<string>({ required: true });
+const model = defineModel<string>({ required: true });
 
 const input = ref<HTMLInputElement | null>(null);
 
@@ -17,7 +17,7 @@ defineExpose({ focus: () => input.value?.focus() });
 <template>
     <input
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        v-model="modelValue"
+        v-model="model"
         ref="input"
     />
 </template>
--- stubs/inertia-vue/resources/js/Components/TextInput.vue
+++ stubs/inertia-vue/resources/js/Components/TextInput.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-const modelValue = defineModel({
+const model = defineModel({
     type: String,
     required: true,
 });
@@ -20,7 +20,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        v-model="modelValue"
+        v-model="model"
         ref="input"
     />
 </template>
