a8d460fdb0e1da7ecee203432f1e2bcd2d6578eb
Matthias Weiß <mail@matthiasweiss.at>
2024-01-13 13:58:57 +0100

    Add client site hydration to React stubs

--- stubs/inertia-react/resources/js/app.jsx
+++ stubs/inertia-react/resources/js/app.jsx
@@ -11,9 +11,7 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        hydrateRoot(el, <App {...props} />);
     },
     progress: {
         color: '#4B5563',
