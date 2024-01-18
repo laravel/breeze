76042a97e1481906829fc3fdcbfabe20cb28945b
Nuno Maduro <enunomaduro@gmail.com>
2024-01-18 12:54:21 +0000

    Slim comments

--- stubs/default/routes/web.php
+++ stubs/default/routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded within the "web" middleware group which includes
-| sessions, cookie encryption, and more. Go build something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
