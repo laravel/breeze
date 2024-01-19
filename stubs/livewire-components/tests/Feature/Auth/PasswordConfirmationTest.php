cfbf6765458aedd147a95fd255d5acdea57f44f1
Douglas Ferreira <douglas.frc@hotmail.com>
2024-01-19 13:33:44 -0300

    Add RouteServiceProvider to PasswordConfirmationTest

--- stubs/livewire-components/tests/Feature/Auth/PasswordConfirmationTest.php
+++ stubs/livewire-components/tests/Feature/Auth/PasswordConfirmationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
