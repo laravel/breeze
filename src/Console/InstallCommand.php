637d894a15caec1e62c752e3d4e77380f7f921ba
Nuno Maduro <enunomaduro@gmail.com>
2024-01-29 20:17:45 +0000

    wip

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -388,7 +388,7 @@ class InstallCommand extends Command implements PromptsForMissingInput
 
         $input->setOption('pest', select(
             label: 'Which testing framework do you prefer?',
-            options: ['PHPUnit', 'Pest'],
+            options: ['Pest', 'PHPUnit'],
             default: $this->isUsingPest() ? 'Pest' : 'PHPUnit',
         ) === 'Pest');
     }
