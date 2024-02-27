21dbc9a0dc3681136f2dc17cb030887f421c5d81
戴日东 <550398445@qq.com>
2024-02-27 14:25:45 +0800

    fix livewire login form bugs

--- stubs/livewire-functional/resources/views/livewire/pages/auth/login.blade.php
+++ stubs/livewire-functional/resources/views/livewire/pages/auth/login.blade.php
@@ -32,7 +32,7 @@ $login = function () {
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+            <x-input-error :messages="array_merge($errors->get('form.email'), $errors->get('email'))" class="mt-2" />
         </div>
 
         <!-- Password -->
--- stubs/livewire/resources/views/livewire/pages/auth/login.blade.php
+++ stubs/livewire/resources/views/livewire/pages/auth/login.blade.php
@@ -34,7 +34,7 @@ new #[Layout('layouts.guest')] class extends Component
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+            <x-input-error :messages="array_merge($errors->get('form.email'), $errors->get('email'))" class="mt-2" />
         </div>
 
         <!-- Password -->
