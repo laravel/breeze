e2b1e7124ce859ae3b366c399eb7a22655f23707
Jason Beggs <jason@roasted.dev>
2024-02-27 21:11:04 -0500

    Implement new welcome page design (#362)

--- stubs/livewire-functional/resources/views/livewire/welcome/navigation.blade.php
+++ stubs/livewire-functional/resources/views/livewire/welcome/navigation.blade.php
@@ -1,11 +1,26 @@
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-end z-10">
+<nav class="-mx-3 flex flex-1 justify-end">
     @auth
-        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
+        <a
+            href="{{ url('/dashboard') }}"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+        >
+            Dashboard
+        </a>
     @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
+        <a
+            href="{{ route('login') }}"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+        >
+            Log in
+        </a>
 
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</a>
+            <a
+                href="{{ route('register') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+            >
+                Register
+            </a>
         @endif
     @endauth
-</div>
+</nav>
