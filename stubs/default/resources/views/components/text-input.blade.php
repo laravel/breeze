a14f21349fe0ff8a77c327ac438bed4268287701
skdishansachin <skdishansachin@gmail.com>
2024-03-02 19:13:47 +0530

    Update text-input blade component with disabled styles

--- stubs/default/resources/views/components/text-input.blade.php
+++ stubs/default/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm disabled:text-gray-500 disabled:dark:text-gray-500 disabled:border-gray-300 disabled:dark:border-gray-700 disabled:bg-gray-50 disabled:dark:bg-gray-900 disabled:cursor-not-allowed']) !!}>
