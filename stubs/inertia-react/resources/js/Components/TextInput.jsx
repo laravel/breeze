5773ecb64f90b5f5ef630af8a04295e37320cabd
skdishansachin <skdishansachin@gmail.com>
2024-03-02 19:14:45 +0530

    Add disabled styles to react TextInput component

--- stubs/inertia-react/resources/js/Components/TextInput.jsx
+++ stubs/inertia-react/resources/js/Components/TextInput.jsx
@@ -14,7 +14,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
             {...props}
             type={type}
             className={
-                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm disabled:text-gray-500 disabled:dark:text-gray-500 disabled:border-gray-300 disabled:dark:border-gray-700 disabled:bg-gray-50 disabled:dark:bg-gray-900 disabled:cursor-not-allowed ' +
                 className
             }
             ref={input}
