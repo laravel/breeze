ee266dab4be32631cbb6d64a922ab330da4a2c16
sohailahmad <sohailahmad0757@gmail.com>
2024-03-14 04:35:06 +0500

    forgot to change the profile views name

--- stubs/livewire/resources/views/profile.blade.php
+++ stubs/livewire/resources/views/profile.blade.php
@@ -9,19 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.update-profile-information-form />
+                    <livewire:profile.update-profile-information />
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.update-password-form />
+                    <livewire:profile.update-password />
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <livewire:profile.delete-user-form />
+                    <livewire:profile.delete-user />
                 </div>
             </div>
         </div>
