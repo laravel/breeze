94ab72bf49b6f834d7661733d953e2b689040bb6
Jess Archer <jess@jessarcher.com>
2024-04-24 15:29:06 +1000

    Fix JSX syntax

--- stubs/inertia-react/resources/js/Pages/Welcome.jsx
+++ stubs/inertia-react/resources/js/Pages/Welcome.jsx
@@ -124,11 +124,11 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 24 24"
-                                            stroke-width="1.5"
+                                            strokeWidth="1.5"
                                         >
                                             <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
                                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                                             />
                                         </svg>
@@ -167,11 +167,11 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
                                             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                                         />
                                     </svg>
@@ -213,11 +213,11 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
                                             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                                         />
                                     </svg>
