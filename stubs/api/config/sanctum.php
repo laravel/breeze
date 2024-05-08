261b39665de07249934a9cb84e396a66d60f5028
Cyrrr <72866514+0xCyyy3000@users.noreply.github.com>
2024-05-08 10:48:44 +0800

    Update sanctum.php
    Get FRONTEND_URL with port provided from .env

--- stubs/api/config/sanctum.php
+++ stubs/api/config/sanctum.php
@@ -19,7 +19,7 @@ return [
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
+        env('FRONTEND_URL') ? ',' . parse_url(env('FRONTEND_URL'), PHP_URL_HOST) . (parse_url(env('FRONTEND_URL'), PHP_URL_PORT) ? ':' . parse_url(env('FRONTEND_URL'), PHP_URL_PORT) : '') : ''
     ))),
 
     /*
