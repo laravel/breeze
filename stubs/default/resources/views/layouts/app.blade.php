0f18896ba88d12984c96a720d49b82f0bb678d05
Dasun Tharanga <hello@dasun.dev>
2024-05-28 16:30:08 +0530

    feat: use `@isset()`

--- stubs/default/resources/views/layouts/app.blade.php
+++ stubs/default/resources/views/layouts/app.blade.php
@@ -19,13 +19,13 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @if (isset($header))
+            @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endisset
 
             <!-- Page Content -->
             <main>
