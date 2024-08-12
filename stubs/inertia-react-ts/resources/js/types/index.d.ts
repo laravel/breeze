ef237081cc9b87c858e9de8e35b523f93ac74fd3
Ninja <yazjallad@gmail.com>
2024-08-12 15:06:50 -0700

    Update User type to match db schema (#399)
    * Update index.d.ts
    
    * Update index.d.ts

--- stubs/inertia-react-ts/resources/js/types/index.d.ts
+++ stubs/inertia-react-ts/resources/js/types/index.d.ts
@@ -2,7 +2,7 @@ export interface User {
     id: number;
     name: string;
     email: string;
-    email_verified_at: string;
+    email_verified_at?: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
