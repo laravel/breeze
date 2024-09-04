d736b1e825607b46044c64cd9aa62287d32d8d66
Tim MacDonald <hello@timacdonald.me>
2024-09-04 11:30:12 +1000

    Use Pest as default testing framework

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -391,7 +391,7 @@ class InstallCommand extends Command implements PromptsForMissingInput
         $input->setOption('pest', select(
             label: 'Which testing framework do you prefer?',
             options: ['Pest', 'PHPUnit'],
-            default: $this->isUsingPest() ? 'Pest' : 'PHPUnit',
+            default: 'Pest',
         ) === 'Pest');
     }
 
