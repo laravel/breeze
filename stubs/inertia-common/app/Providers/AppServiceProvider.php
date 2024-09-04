a2f49a350d89e5c569c79bd63ce3faab5a632117
Tim MacDonald <hello@timacdonald.me>
2024-09-05 00:10:27 +1000

    Eagerly prefetch assets (#400)

---
+++ stubs/inertia-common/app/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
+    }
+}
