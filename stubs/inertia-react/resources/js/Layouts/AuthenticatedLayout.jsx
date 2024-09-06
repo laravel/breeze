697502429a9e492d794362e2215882f628d857c8
Matheus Santos <miliandroxd@gmail.com>
2024-09-05 21:07:13 -0300

    missing import

--- stubs/inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
+++ stubs/inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,7 +3,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Link } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function Authenticated({ header, children }) {
     const user = usePage().props.auth.user;
