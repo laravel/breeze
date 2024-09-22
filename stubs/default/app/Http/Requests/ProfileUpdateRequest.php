296d88c52b040b44b1003655af1689ea7dc2c7f1
Amidope <66752876+Amidope@users.noreply.github.com>
2024-09-22 20:02:57 +0500

    Break long email validation rule into multiple lines (#416)
    Co-authored-by: Amidope <noreply>

--- stubs/default/app/Http/Requests/ProfileUpdateRequest.php
+++ stubs/default/app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,14 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
         ];
     }
 }
