196f40f3aa9d1a8c86575f55dbada8971cfdb3fe
msamgan <mohdsamgankhan@gmail.com>
2024-10-05 17:32:07 -0400

    updated names of the default functions to match the file names.

--- stubs/inertia-react/resources/js/Layouts/GuestLayout.jsx
+++ stubs/inertia-react/resources/js/Layouts/GuestLayout.jsx
@@ -1,7 +1,7 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 
-export default function Guest({ children }) {
+export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
