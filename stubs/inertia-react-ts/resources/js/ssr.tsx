9d4e8be48d8c8d6092ec021b688bfc7b42eedeff
AkrAm <ak.khan.kp@gmail.com>
2024-10-17 17:46:52 +0600

    fix: TypeScript Errors in SSR Setup with Ziggy Props when Builds

--- stubs/inertia-react-ts/resources/js/ssr.tsx
+++ stubs/inertia-react-ts/resources/js/ssr.tsx
@@ -22,7 +22,9 @@ createServer((page) =>
             // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
+                    // @ts-expect-error
                     ...page.props.ziggy,
+                    // @ts-expect-error
                     location: new URL(page.props.ziggy.location),
                 });
             /* eslint-enable */
