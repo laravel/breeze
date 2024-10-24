28897a0fa6991b6ecc6cac7a07468fe74d56535c
Shah Alam <43747088+ShahAlamShaikat@users.noreply.github.com>
2024-10-24 15:06:21 +0100

    Update sanctum.php (#431)
    fix(sanctum): add 127.0.0.1:3000 for proper API auth during local development with 127.0.0.1:3000

--- stubs/api/config/sanctum.php
+++ stubs/api/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
