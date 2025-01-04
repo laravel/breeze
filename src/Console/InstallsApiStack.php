f993139c62d70bed075014ae5c90af306676bb8f
Shihab Mahamud <shihab4t@gmail.com>
2025-01-04 14:15:51 +0600

    remove postcss config js file

--- src/Console/InstallsApiStack.php
+++ src/Console/InstallsApiStack.php
@@ -83,6 +83,7 @@ trait InstallsApiStack
         $files->delete(base_path('package.json'));
         $files->delete(base_path('vite.config.js'));
         $files->delete(base_path('tailwind.config.js'));
+        $files->delete(base_path('postcss.config.js'));
 
         // Remove Laravel "welcome" view...
         $files->delete(resource_path('views/welcome.blade.php'));
