650f8695f1cd30bdd5340150ac72f4a63c4b71e1
Joel Butcher <joelbutcher@users.noreply.github.com>
2025-01-17 19:18:55 +0000

    add types to useForm in Login.tsx

--- stubs/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
+++ stubs/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
@@ -8,7 +8,11 @@ import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }: { status?: string, canResetPassword: boolean }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
+    const { data, setData, post, processing, errors, reset } = useForm<{
+        email: string;
+        password: string;
+        remember: boolean;
+    }>({
         email: '',
         password: '',
         remember: false,
