9f983a014ed16b23de5794e6ca55e25cff8f63fe
Ashok <ashokbaruaakas@gmail.com>
2025-01-20 10:23:25 +0600

    Missing type declaration add to NewPasswordController.php
    Add missing type to get 100% type coverage... 🚀

--- stubs/default/app/Http/Controllers/Auth/NewPasswordController.php
+++ stubs/default/app/Http/Controllers/Auth/NewPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -40,7 +41,7 @@ class NewPasswordController extends Controller
         // database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
-            function ($user) use ($request) {
+            function (User $user) use ($request) {
                 $user->forceFill([
                     'password' => Hash::make($request->password),
                     'remember_token' => Str::random(60),
