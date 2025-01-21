21da08bb8a14221dffe4cd4abd838b3d9269a1b4
Chikondi Kamwendo <kondie208@gmail.com>
2025-01-21 07:00:30 +0200

    [2.x] Added pagination type definitions to react-ts.

--- stubs/inertia-react-ts/resources/js/types/index.d.ts
+++ stubs/inertia-react-ts/resources/js/types/index.d.ts
@@ -6,9 +6,31 @@ export interface User {
 }
 
 export type PageProps<
-    T extends Record<string, unknown> = Record<string, unknown>,
+    T extends Record<string, unknown> = Record<string, unknown>
 > = T & {
     auth: {
         user: User;
     };
+};
+
+export type PaginationLink = {
+    url: string | null;
+    label: string;
+    active: boolean;
+};
+
+export type Pagination<T> = {
+    data: Array<T>;
+    links: Array<PaginationLink>;
+    path: string;
+    from: number;
+    to: number;
+    total: number;
+    current_page: number;
+    last_page: number;
+    per_page: number;
+    first_page_url: string;
+    last_page_url: string;
+    next_page_url: string | null;
+    prev_page_url: string | null;
 };
