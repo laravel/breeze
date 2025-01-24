cefa1357a1642263341dfcfaaede714678d5c576
Flori <kodraflorian@gmail.com>
2025-01-24 15:56:57 +0200

    Fix type error in Login component for `setData` function
    - Resolved TS2345 error by ensuring `setData` accepts a boolean type.
    - Updated the `onChange` handler in the `Checkbox` component to handle `e.target.checked` correctly.
    - Improved type safety and compatibility for the `remember` field.

--- stubs/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
+++ stubs/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
@@ -78,7 +78,7 @@ export default function Login({
                             name="remember"
                             checked={data.remember}
                             onChange={(e) =>
-                                setData('remember', e.target.checked)
+                                setData('remember', (e.target.checked || false) as false)
                             }
                         />
                         <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">
