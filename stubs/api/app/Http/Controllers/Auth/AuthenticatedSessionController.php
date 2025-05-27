9569726aa376b81541c12e3b2c6884e4b8244555
MohammadRezae <mohammaddevrezaei@gmail.com>
2025-05-27 18:50:41 +0330

    refactor(AuthenticatedSession): Add detailed docblocks to AuthenticatedSessionController

--- stubs/api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ stubs/api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -12,6 +12,9 @@ class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(LoginRequest $request): Response
     {
@@ -24,6 +27,9 @@ class AuthenticatedSessionController extends Controller
 
     /**
      * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Request $request): Response
     {
