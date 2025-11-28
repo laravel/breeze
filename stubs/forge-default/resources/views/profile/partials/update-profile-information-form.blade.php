9934512a0d2262371aa234e076392dde0edc5af2
rcravens <bob.cravens@gmail.com>
2025-11-28 17:45:03 -0600

    updated verb to match fortify route

--- stubs/forge-default/resources/views/profile/partials/update-profile-information-form.blade.php
+++ stubs/forge-default/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -15,7 +15,7 @@
 
     <form method="post" action="{{ route('user-profile-information.update') }}" class="mt-6 space-y-6">
         @csrf
-        @method('patch')
+        @method('put')
 
         <div>
             <x-input-label for="name" :value="__('Name')"/>
